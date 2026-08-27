c49026bec01704e403116874634b55d2c94bfa2d
Jobbazs <grega.balazs@gmail.com>
2026-08-27 18:36:16 +0200

    termekismerteto patch 3

--- src/components/Termekismerteto.jsx
+++ src/components/Termekismerteto.jsx
@@ -150,9 +150,10 @@ export default function Termekismerteto() {
 
           <footer className="ti-footer">
             <p className="ti-outro">Kérdésed van, vagy konkrét ajánlatot szeretnél a saját vállalkozásodra? Keress bizalommal.</p>
+            <br />
             <p className="ti-outro-dev">Fejlesztő: Grega Balázs</p>
             <p className="ti-outro-dev">Email cím: balazsgregadev@gmail.com</p>
-            
+            <br />
             <a className="ti-download" href="/Termekismerteto.docx" download>Letöltés Word-dokumentumként</a>
           </footer>
         </article>
